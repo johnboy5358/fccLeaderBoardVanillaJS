1469496ef18b353a2644b14e9bc3004629bd5c09
johnboy5358 <johnswindin@gmail.com>
2018-03-08 16:33:54 +0000

    Updated app.js: A step towards es5 compatibility

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
-!(function(){
+(function(){
 
   // "use strict"
-  const tableWrapper = document.getElementById('table-wrapper')
+  var tableWrapper = document.getElementById('table-wrapper')
   try {
 
     // decoupled and curried helper functions.
     // const map = fn => coll => Array.prototype.map.call(coll, fn)
-    var map = function map(fn) {
+    function map(fn) {
       return function (coll) {
         return Array.prototype.map.call(coll, fn);
       };
-    };
+    }
 
     // const join = init => data => Array.prototype.join.call(data, init)
-    var join = function join(init) {
+    function join(init) {
       return function (data) {
         return Array.prototype.join.call(data, init);
       };
-    };
+    }
 
     // const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x)
-    var pipe = function pipe() {
+    function pipe() {
       for (var _len = arguments.length, fns = Array(_len), _key = 0; _key < _len; _key++) {
         fns[_key] = arguments[_key];
       }
@@ -30,17 +30,17 @@
           return f(v);
         }, x);
       };
-    };
+    }
 
     function getTopUsers(baseUrl){
       return async function (type){
-        return await fetch(baseUrl+type).then(response => response.json())
+        return await fetch(baseUrl+type).then(function(response){ return response.json()})
      }
     }
 
     // fn :: template constructs each user table row.
     function tableRowTemplate(user, i){
-      let url = `http://www.freecodecamp.org/${user.username}`
+      var url = `http://www.freecodecamp.org/${user.username}`
       return (`
         <tr>
           <td class="rank">${i+1}</td>
@@ -52,9 +52,10 @@
         </tr>`)
     }
 
-    const makeBtnClickEvt = (targ, listen, dataString) => {
+    function makeBtnClickEvt(targ, listen, dataString) {
 
-      const listener = document.querySelector(listen)
+      var listener = document.querySelector(listen)
+      
       listener.addEventListener('click', function(e){
         listener.parentElement.parentElement.className = listen.slice(1)
         targ.innerHTML = dataString
@@ -64,14 +65,14 @@
   
     (async function () {
 
-      const transformAryToHtmlString = pipe(
+      var transformAryToHtmlString = pipe(
         map(tableRowTemplate),
         join('')
       )
 
       // get data from api.
-      const recentData = await getTopUsers('https://fcctop100.herokuapp.com/api/fccusers/top/')('recent')
-      const allTimeData = await getTopUsers('https://fcctop100.herokuapp.com/api/fccusers/top/')('alltime')
+      var recentData = await getTopUsers('https://fcctop100.herokuapp.com/api/fccusers/top/')('recent')
+      var allTimeData = await getTopUsers('https://fcctop100.herokuapp.com/api/fccusers/top/')('alltime')
 
       // Initialise table.
       tableWrapper.innerHTML = transformAryToHtmlString(recentData)
